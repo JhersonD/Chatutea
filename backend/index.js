3d018fce09318a24ce549fd84192abb5a39298e9
Jherson Chavez <jhersondannychavezmontes@gmail.com>
2024-04-14 19:41:34 -0500

    las

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const axios = require("axios");
 
 const app = express();
 app.use(express.json());
+app.use(cors({ origin: true , headers: "*"}));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
