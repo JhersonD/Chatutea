913696ea3d4060fe6f8032fb788ef779e3dc646d
Jherson Chavez <jhersondannychavezmontes@gmail.com>
2024-04-14 19:48:06 -0500

    a

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: true , headers: "*"}));
+app.use(cors({ origin: "https://chatutea-frontend-j4mq.vercel.app/" , methods: ["GET","POST"]}));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
